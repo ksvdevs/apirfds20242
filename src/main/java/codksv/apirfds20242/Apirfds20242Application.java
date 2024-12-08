d032fb240af1eff5b0c109e1632e47d8cf20cc94
ksvdevs <ksegundo@unamba.edu.pe>
2024-12-08 18:35:38 -0500

    feat: Initialization

---
+++ src/main/java/codksv/apirfds20242/Apirfds20242Application.java
@@ -0,0 +1,13 @@
+package codksv.apirfds20242;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Apirfds20242Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Apirfds20242Application.class, args);
+	}
+
+}
---
+++ src/test/java/codksv/apirfds20242/Apirfds20242ApplicationTests.java
@@ -0,0 +1,13 @@
+package codksv.apirfds20242;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Apirfds20242ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
